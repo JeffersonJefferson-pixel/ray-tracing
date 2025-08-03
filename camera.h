6989aa151bb9417ac7848d4e571b7d223d4b68cd
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2025-08-03 15:54:57 +0700

    add scattering pdf for lambertian material

--- camera.h
+++ camera.h
@@ -153,7 +153,12 @@ class camera {
                 return color_from_emission;
             }
             
-            color color_from_scatter = attenuation * ray_color(scattered, depth-1, world);
+
+            // pdf
+            double scattering_pdf = rec.mat->scattering_pdf(r, rec, scattered);
+            double pdf_value = scattering_pdf;
+
+            color color_from_scatter = attenuation * scattering_pdf * ray_color(scattered, depth-1, world);
 
             return color_from_emission + color_from_scatter;
         }
